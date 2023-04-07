1120ad446a4a4dd0327e19a45a42af26e5c91b1d
mehran vejdani <mehran.vejdanii@gmail.com>
2023-04-07 16:54:58 +0430

    add event to input selector

---
+++ script.js
@@ -0,0 +1,10 @@
+const input = document.querySelector(".input");
+const grid = document.querySelector(".grid");
+
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    loadImg();
+  }
+});
+
+function loadImg() {}
