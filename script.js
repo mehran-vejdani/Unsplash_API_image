77c5b2c4f937d98827ba19d0448e3009ee7e0462
mehran vejdani <mehran.vejdanii@gmail.com>
2023-04-07 16:59:53 +0430

    add dayNightMode function for change the color of site

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input = document.querySelector(".input");
 const grid = document.querySelector(".grid");
 
+window.addEventListener("load", dayNightMode);
 input.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     loadImg();
@@ -8,3 +9,16 @@ input.addEventListener("keydown", (event) => {
 });
 
 function loadImg() {}
+
+function dayNightMode() {
+  const data = new Date();
+  const hour = data.getHours();
+
+  if (hour >= 7 && hour <= 19) {
+    document.body.style.backgroundColor = "whitesmoke";
+    document.body.style.color = "black";
+  } else {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+  }
+}
